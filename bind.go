fa693fa38f7ec258143b42a39fc5b93e318d28c4
Gurleen Sethi <sarusethi@rocketmail.com>
2026-07-01 00:25:19 -0400

    deduplicate pointer/non-pointer branches via setScalar helper

--- bind.go
+++ bind.go
@@ -83,165 +83,56 @@ func Handler[Req any, Res any](next HandlerFunc[Req, Res]) http.Handler {
 	})
 }
 
-func setFieldValue(val reflect.Value, valType reflect.StructField, value string) {
-	// Empty values are ignored.
-	// Non-pointer values will have default zero values.
-	// Pointer values will stay nil.
+func setFieldValue(val reflect.Value, _ reflect.StructField, value string) {
 	if value == "" {
 		return
 	}
 
 	if val.Kind() == reflect.Pointer {
-		switch val.Type().Elem().Kind() {
-		case reflect.String:
-			val.Set(reflect.ValueOf(&value))
-		case reflect.Bool:
-			boolVal, err := strconv.ParseBool(value)
-			if err == nil {
-				val.Set(reflect.ValueOf(&boolVal))
-			}
-		case reflect.Int8:
-			intVal, err := strconv.ParseInt(value, 10, 8)
-			if err == nil {
-				i := int8(intVal)
-				val.Set(reflect.ValueOf(&i))
-			}
-		case reflect.Int16:
-			intVal, err := strconv.ParseInt(value, 10, 16)
-			if err == nil {
-				i := int16(intVal)
-				val.Set(reflect.ValueOf(&i))
-			}
-		case reflect.Int32:
-			intVal, err := strconv.ParseInt(value, 10, 32)
-			if err == nil {
-				i := int32(intVal)
-				val.Set(reflect.ValueOf(&i))
-			}
-		case reflect.Int64:
-			intVal, err := strconv.ParseInt(value, 10, 64)
-			if err == nil {
-				val.Set(reflect.ValueOf(&intVal))
-			}
-		case reflect.Int:
-			intVal, err := strconv.Atoi(value)
-			if err == nil {
-				val.Set(reflect.ValueOf(&intVal))
-			} else {
-				slog.Warn(value + " is not int")
-			}
-		case reflect.Uint8:
-			uintVal, err := strconv.ParseUint(value, 10, 8)
-			if err == nil {
-				i := uint8(uintVal)
-				val.Set(reflect.ValueOf(&i))
-			}
-		case reflect.Uint16:
-			uintVal, err := strconv.ParseUint(value, 10, 16)
-			if err == nil {
-				i := uint16(uintVal)
-				val.Set(reflect.ValueOf(&i))
-			}
-		case reflect.Uint32:
-			uintVal, err := strconv.ParseUint(value, 10, 32)
-			if err == nil {
-				i := uint32(uintVal)
-				val.Set(reflect.ValueOf(&i))
-			}
-		case reflect.Uint:
-			uintVal, err := strconv.ParseUint(value, 10, 0)
-			if err == nil {
-				i := uint(uintVal)
-				val.Set(reflect.ValueOf(&i))
-			}
-		case reflect.Uint64:
-			uintVal, err := strconv.ParseUint(value, 10, 64)
-			if err == nil {
-				val.Set(reflect.ValueOf(&uintVal))
-			}
-		case reflect.Float32:
-			floatVal, err := strconv.ParseFloat(value, 32)
-			if err == nil {
-				f := float32(floatVal)
-				val.Set(reflect.ValueOf(&f))
-			}
-		case reflect.Float64:
-			floatVal, err := strconv.ParseFloat(value, 64)
-			if err == nil {
-				val.Set(reflect.ValueOf(&floatVal))
-			}
+		ptr := reflect.New(val.Type().Elem())
+		setScalar(ptr.Elem(), value)
+		val.Set(ptr)
+		return
+	}
+
+	setScalar(val, value)
+}
+
+func setScalar(val reflect.Value, value string) {
+	switch val.Kind() {
+	case reflect.String:
+		val.SetString(value)
+	case reflect.Bool:
+		boolVal, err := strconv.ParseBool(value)
+		if err == nil {
+			val.SetBool(boolVal)
 		}
-	} else {
-		switch val.Kind() {
-		case reflect.String:
-			val.SetString(value)
-		case reflect.Bool:
-			boolVal, err := strconv.ParseBool(value)
-			if err == nil {
-				val.SetBool(boolVal)
-			}
-		case reflect.Int8:
-			intVal, err := strconv.ParseInt(value, 10, 8)
-			if err == nil {
-				val.SetInt(intVal)
-			}
-		case reflect.Int16:
-			intVal, err := strconv.ParseInt(value, 10, 16)
-			if err == nil {
-				val.SetInt(intVal)
-			}
-		case reflect.Int32:
-			intVal, err := strconv.ParseInt(value, 10, 32)
-			if err == nil {
-				val.SetInt(intVal)
-			}
-		case reflect.Int64:
-			intVal, err := strconv.ParseInt(value, 10, 64)
-			if err == nil {
-				val.SetInt(intVal)
-			}
-		case reflect.Int:
-			intVal, err := strconv.Atoi(value)
-			if err == nil {
-				val.SetInt(int64(intVal))
-			} else {
-				slog.Warn(value + " is not int")
-			}
-		case reflect.Uint8:
-			uintVal, err := strconv.ParseUint(value, 10, 8)
-			if err == nil {
-				val.SetUint(uintVal)
-			}
-		case reflect.Uint16:
-			uintVal, err := strconv.ParseUint(value, 10, 16)
-			if err == nil {
-				val.SetUint(uintVal)
-			}
-		case reflect.Uint32:
-			uintVal, err := strconv.ParseUint(value, 10, 32)
-			if err == nil {
-				val.SetUint(uintVal)
-			}
-		case reflect.Uint:
-			uintVal, err := strconv.ParseUint(value, 10, 0)
-			if err == nil {
-				val.SetUint(uintVal)
-			}
-		case reflect.Uint64:
-			uintVal, err := strconv.ParseUint(value, 10, 64)
-			if err == nil {
-				val.SetUint(uintVal)
-			}
-		case reflect.Float32:
-			floatVal, err := strconv.ParseFloat(value, 32)
-			if err == nil {
-				val.SetFloat(floatVal)
-			}
-		case reflect.Float64:
-			floatVal, err := strconv.ParseFloat(value, 64)
-			if err == nil {
-				val.SetFloat(floatVal)
-			}
+	case reflect.Int:
+		intVal, err := strconv.Atoi(value)
+		if err == nil {
+			val.SetInt(int64(intVal))
+		} else {
+			slog.Warn(value + " is not int")
+		}
+	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		intVal, err := strconv.ParseInt(value, 10, val.Type().Bits())
+		if err == nil {
+			val.SetInt(intVal)
+		}
+	case reflect.Uint:
+		uintVal, err := strconv.ParseUint(value, 10, 0)
+		if err == nil {
+			val.SetUint(uintVal)
+		}
+	case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		uintVal, err := strconv.ParseUint(value, 10, val.Type().Bits())
+		if err == nil {
+			val.SetUint(uintVal)
+		}
+	case reflect.Float32, reflect.Float64:
+		floatVal, err := strconv.ParseFloat(value, val.Type().Bits())
+		if err == nil {
+			val.SetFloat(floatVal)
 		}
 	}
 }
